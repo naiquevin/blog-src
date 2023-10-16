f81373fde50755cdc3b1cc117508eb4241a5824b
Vineet Naik <naikvin@gmail.com>
2023-10-16 17:44:56 +0530

    Add favicon.ico

--- pelicanconf.py
+++ pelicanconf.py
@@ -48,7 +48,7 @@ MARKDOWN = {
     'output_format': 'html5'
 }
 
-STATIC_PATHS = ['talks']
+STATIC_PATHS = ['talks', 'extra']
 
 PAGE_EXCLUDES = ['talks']
 ARTICLE_EXCLUDES = ['pages', 'talks']
@@ -64,4 +64,8 @@ MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/index.html'
 # published` has to be explicitly added in the metadata
 DEFAULT_METADATA = {
     'status': 'draft'
+}
+
+EXTRA_PATH_METADATA = {
+    'extra/favicon.ico': {'path': 'favicon.ico'},
 }
